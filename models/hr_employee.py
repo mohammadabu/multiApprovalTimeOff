95627c320c42699cb6747481cea6514a4eada400
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-17 01:07:15 +0300

    e

---
+++ models/hr_employee.py
@@ -0,0 +1,12 @@
+import re
+from datetime import datetime, timedelta
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError, AccessError, ValidationError
+from odoo.tools import email_split
+from pytz import timezone, UTC
+
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'
+    commencement_business = fields.Date("Date of commencement of business")
+    
