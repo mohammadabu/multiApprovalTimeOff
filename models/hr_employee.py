c2cab3f55ac4ad2803210b2d3ad354c78c7e0c27
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-18 14:25:01 +0300

    Update hr_employee.py

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -12,21 +12,26 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     commencement_business = fields.Date("Date of commencement of business")
 
-    # @api.model
-    # def create(self,vals):
-    #     try:   
-    #         _logger.info("---------------------------")
-    #         _logger.info(vals['commencement_business'])
-    #         commencement_business = vals['commencement_business']
-    #         annual_leave_type = self.env['hr.leave.type'].sudo().search(['&',('finished_carry_froword','=',False),'|',('validity_stop','>=',commencement_business),'&',('validity_start','=',False),('validity_stop','=',False)])
-    #         _logger.info(annual_leave_type)
-    #         for annual in annual_leave_type:
-    #             annual_leave_allocation = self.env['hr.leave.allocation'].sudo().search([('holiday_status_id','=',annual.id),('state','=','validate')])
-
+    @api.model
+    def create(self,vals):
+        try:   
+            _logger.info("---------------------------")
+            _logger.info(vals['commencement_business'])
+            _logger.info(vals['company_id'])
+            commencement_business = vals['commencement_business']
+            annual_leave_type = self.env['hr.leave.type'].sudo().search(['&',('finished_carry_froword','=',False),'|',('validity_stop','>=',commencement_business),'&',('validity_start','=',False),('validity_stop','=',False)])
+            _logger.info(annual_leave_type)
+            for annual in annual_leave_type:
+                annual_leave_allocation = self.env['hr.leave.allocation'].sudo().search([('holiday_status_id','=',annual.id),('state','=','validate')])
+                for allocation in annual_leave_allocation:
+                    # By Company
+                    if allocation.holiday_type == "company":
+                        print('sdadsa')
+                 							
                 
-    #     except:
-    #         _logger.info("An exception occurred")                                  
-    #     rtn = super(HrEmployee,self).create(vals)
-    #     return rtn 
+        except:
+            _logger.info("An exception occurred")                                  
+        rtn = super(HrEmployee,self).create(vals)
+        return rtn 
 
 
