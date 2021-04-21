45247fe1a6d785e3d28854861ba01b675947be48
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-21 03:07:54 +0300

    Update hrAllocations.py

--- models/hrAllocations.py
+++ models/hrAllocations.py
@@ -19,11 +19,11 @@ class HrAllocations(models.Model):
         for annual in annual_leave_allocation:
             if annual.employee_id != False:
                 if annual.employee_id.commencement_business != False:
+                    annual_leave_type = self.env['hr.leave.type'].sudo().search(['&',('id','=',annual.holiday_status_id.id),'&',('validity_start','!=',False),('validity_stop','!=',False)])
                     commencement_business = annual.employee_id.commencement_business
                     commencement_business = datetime.strptime(str(commencement_business),'%Y-%m-%d').date()
                     validity_stop = annual_leave_type.validity_stop
                     statment_1 = (validity_stop - commencement_business).days
-                    annual_leave_type = self.env['hr.leave.type'].sudo().search(['&',('id','=',annual.holiday_status_id.id),'&',('validity_start','!=',False),('validity_stop','!=',False)])
                     _logger.info("------------------------")
                     _logger.info(annual.employee_id.name)
                     _logger.info(commencement_business)
