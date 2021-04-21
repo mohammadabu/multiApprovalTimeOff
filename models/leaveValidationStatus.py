fc25149d575e80f96978b5189eaffda3c09f2cd8
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-04-21 14:06:01 +0300

    e

--- models/leaveValidationStatus.py
+++ models/leaveValidationStatus.py
@@ -30,6 +30,25 @@ class LeaveValidationStatus(models.Model):
                                        default=False,
                                        track_visibility='always', help="Refused")                                   
     leave_comments = fields.Text(string='Comments', help="Comments",readonly=True)
+    exceptions = fields.Selection(
+        [
+            ('1d','Greater than  1 day'),
+            ('2d','Greater than  2 day'),
+            ('3d','Greater than  3 day'),
+            ('4d','Greater than  4 day'),
+            ('5d','Greater than  5 day'),
+            ('6d','Greater than  6 day'),
+            ('7d','Greater than  7 day'),
+            ('8d','Greater than  8 day'),
+            ('9d','Greater than  9 day'),
+            ('10d','Greater than  10 day'),
+            ('11d','Greater than  11 day'),
+            ('12d','Greater than  12 day'),
+            ('13d','Greater than  13 day'),
+            ('14d','Greater than  14 day'),
+            ('15d','Greater than  15 day'),
+        ]
+    )
 
     @api.onchange('validators_type','holiday_validators_user','holiday_validators_position','approval')
     def prevent_change(self):
