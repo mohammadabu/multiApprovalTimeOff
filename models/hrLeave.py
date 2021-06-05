ada82497500e1eeaaed9c2254df4d6023fe008e7
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-05 12:18:14 +0300

    Update hrLeave.py

--- models/hrLeave.py
+++ models/hrLeave.py
@@ -93,11 +93,11 @@ class HrLeave(models.Model):
         time_off_type = self.env['hr.leave.type'].sudo().search([('id','=',self.holiday_status_id.id)])
         # 5/6/2021
         _logger.info("-------------yearsـofـservice-------------")
-        yearsـofـservice = datetime.strptime(time_off_type.yearsـofـservice,'%Y-%m-%d').date()
+        date_joining = datetime.strptime(self.employee_id.date_joining,'%Y-%m-%d').date()
         now = datetime.strptime(date.today(),'%Y-%m-%d').date()
-        _logger.info(yearsـofـservice)
+        _logger.info(date_joining)
         _logger.info(now)
-        _logger.info(self.employee_id.date_joining)
+        _logger.info(time_off_type.yearsـofـservice)
         _logger.info("-------------yearsـofـservice-------------")
         if time_off_type.yearsـofـservice == 0:
             raise UserError(_(
