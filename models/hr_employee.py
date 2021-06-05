04831c6547f92789d2c6a463a0d9da482e9ad0b8
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-06-05 12:22:36 +0300

    Update hr_employee.py

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,date
 from odoo import models, api, fields, _
 import logging
 _logger = logging.getLogger(__name__)
